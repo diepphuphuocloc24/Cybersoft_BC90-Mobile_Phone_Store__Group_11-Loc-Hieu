457d120f094a6f5e99dc80151ff05011726013a0
diepphuphuocloc24 <diepphuphuocloc24@gmail.com>
2025-10-15 21:44:33 +0700

    Loc update Modal HTML

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-import Product from "./product.js";
 import Manager from "./manager.js";
 
-const product = new Product();
 const manager = new Manager();
--- js/manager.js
+++ js/manager.js
@@ -1,2 +1,11 @@
-class Manager {}
+class Manager {
+  constructor() {
+    this.array_Stock = [];
+  }
+  btn_add(id) {
+    this.array_Stock.push(id);
+  }
+  btn_minus() {}
+  btn_delete() {}
+}
 export default Manager;
